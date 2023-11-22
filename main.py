ddd6e5e8adc4d745596c7aaac330bdbc81ad6fc3
Ваккер Богдан Сергеевич, lab01-executor <Vakker150217@gmail.com>
2023-11-22 19:36:52 +0300

    New ball added and new target

--- main.py
+++ main.py
@@ -49,13 +49,13 @@ class Ball:
         if((self.x+self.r)<800) or(self.vx<0) :
             self.x += self.vx
         else:
-            self.vx=-0.9*self.vx
+            self.vx=-0.6*self.vx
 
-        if((self.y+self.r)<600) or(self.vy<0):
-            self.y += self.vy
+        if((self.y+self.r)<600) or(self.vy>0):
+            self.y -= self.vy
         else:
-            self.vy = -0.9*self.vy
-        self.vy += g
+            self.vy = -0.7*self.vy
+        self.vy -= g
         # self.x += self.vx
         # self.y -= self.vy
         # self
@@ -82,6 +82,29 @@ class Ball:
         else:
             return False
 
+class manyBall():
+    def __init__(self, screen: pygame.Surface, x=40, y=450):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.r = 10
+        self.vx = 0
+        self.vy = 0
+        self.color = choice(GAME_COLORS)
+        self.live = 30
+        self.num=5
+        self.balls=[]
+    def give_balls(self):
+        for i in range(self.num):
+            new_ball=Ball(self.screen)
+            new_ball.vx = self.vx
+            new_ball.vy=self.vy+5-2*i
+            self.balls.append(new_ball)
+        return self.balls
+
+
+
+
 
 class Gun:
     def __init__(self, screen):
@@ -90,24 +113,44 @@ class Gun:
         self.f2_on = 0
         self.an = 1
         self.color = GREY
+        self.x = 40
+        self.y=450
 
     def fire2_start(self, event):
         self.f2_on = 1
 
-    def fire2_end(self, event):
+    def fire2_end(self, event, Kspace):
         """Выстрел мячом.
 
         Происходит при отпускании кнопки мыши.
         Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
         """
         global balls, bullet
-        bullet += 1
-        new_ball = Ball(self.screen)
-        new_ball.r += 5
-        self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
-        new_ball.vx = self.f2_power * math.cos(self.an)
-        new_ball.vy = self.f2_power * math.sin(self.an)
-        balls.append(new_ball)
+
+        # bullet += 1
+        # new_ball = Ball(self.screen)
+        # new_ball.r += 5
+        #self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
+        self.an = math.atan2((event.pos[1] - self.y), (event.pos[0] - self.x))
+        # new_ball.vx = self.f2_power * math.cos(self.an)
+        # new_ball.vy = -self.f2_power * math.sin(self.an)
+        vx = self.f2_power * math.cos(self.an)
+        vy = -self.f2_power * math.sin(self.an)
+        if (Kspace):
+            newmanyball = manyBall(self.screen)
+            newmanyball.vx = vx
+            newmanyball.vy=vy
+            new_balls = newmanyball.give_balls()
+            balls += new_balls
+            bullet+=len(new_balls)
+        else:
+            new_ball = Ball(self.screen)
+            new_ball.vx=vx
+            new_ball.vy=vy
+            bullet += 1
+            balls.append(new_ball)
+
+
         self.f2_on = 0
         self.f2_power = 10
 
@@ -120,18 +163,51 @@ class Gun:
         else:
             self.color = GREY
 
+    # def draw(self):
+    #     width = int(100 + self.f2_power * 0.1)
+    #     height = 4
+    #     self.rectsurface = pygame.Surface((width, height))
+    #     self.rectsurface.fill((255, 0, 0))  # Use RGB tuple for RED
+    #
+    #     self.rect = self.rectsurface.get_rect()
+    #     self.rect.center = (100, 100)
+    #
+    #     # Draw rectangle on the surface
+    #     pygame.draw.rect(self.rectsurface, (0, 255, 0), (0, 0, width, height))  # Use RGB tuple for GREEN
+    #
+    #     self.rotsurf = pygame.transform.rotate(self.rectsurface, 30)  # Adjust the rotation angle
+    #     self.rotrect = self.rotsurf.get_rect()
+    #     self.rotrect.center = (40, 450)
+    #     screen.blit(self.rotsurf, self.rotrect)
     def draw(self):
-        width = int(100+self.f2_power*0.1)
-        height = 4
-        rectsurface = pygame.Surface((width, height), pygame.SRCALPHA)
-        pygame.draw.rect(screen, self.color, (20, 450, width, height))
-
-        # rotsurf = pygame.transform.rotate(rectsurface, 45)
+        pygame.draw.line(
+            self.screen,
+            self.color,
+            (self.x, self.y),
+            (self.x + math.cos(self.an) * (15 + self.f2_power / 2), self.y + math.sin(self.an)
+             * (15 + self.f2_power / 2)),
+            width=7
+        )
+        # width = int(100+self.f2_power*0.1)
+        # height = 50
+        # self.rectsurface = pygame.Surface((width, height))
+        # self.rectsurface.fill(RED)
+        #
+        # self.rect = self.rectsurface.get_rect()
+        # self.rect.center =(100, 100)
+        # pygame.draw.rect(self.rectsurface, self.color, (20,430,50,30))
+        #
+        #
+        # self.rotsurf = pygame.transform.rotate(self.rectsurface, 1)
+        # self.rotrect = self.rotsurf.get_rect()
+        # self.rotrect.center = (40, 450)
+        # screen.blit(self.rotsurf, self.rotrect)
+        # pygame.draw.rect(screen, self.color, (20, 450, width, height))
         # rotrect = rotsurf.get_rect()
         # rotrect.center=(width/2, height/2)
-        # #rect1 = rotrect.get_rect(center=rect.center)
-        # screen.blit(rotsurf, rotrect, (40, 450))
-        # FIXIT don't know how to do it
+        #rect1 = rotrect.get_rect(center=rect.center)
+
+        #FIXIT don't know how to do it
 
     def power_up(self):
         if self.f2_on:
@@ -147,7 +223,8 @@ class Target:
     # self.live = 1
     # FIXME: don't work!!! How to call this functions when object is created?
     # self.new_target()
-    def __init__(self):
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
         self.new_target()
         self.points = 0
         # self.live=1
@@ -170,13 +247,57 @@ class Target:
         x = self.x = randint(600, 780)
         y = self.y = randint(300, 550)
         r = self.r = randint(2, 50)
+        self.vx = 15
+        self.vy = 15
 
     def hit(self, points=1):
         """Попадание шарика в цель."""
         self.points += points
 
+    def move(self):
+        if ((self.x + self.r) >= 800) and (self.vx>0):
+            self.vx= -self.vx
+        elif ((self.x + self.r) <= 200) and (self.vx<0):
+            self.vx = -self.vx
+        else:
+            self.x+=self.vx
+        # if (((self.y + self.r)>590) and (self.vy<0)):
+        #     self.vy = -self.vy
+        # elif (((self.y + self.r)<10) and (self.vy>0)):
+        #     self.vy = -self.vy
+        # else:
+        #     self.y-=self.vy
+
+
+        # if ((self.x + self.r) < 800) or (self.vx < 0):
+        #     self.x += self.vx
+        # else:
+        #     self.vx =  -self.vx
+        #
+        # if ((((self.y + self.r) < 600)) and (self.vy > 0)) or (((self.y+self.r)>200) and (self.vy<0)):
+        #     self.y -= self.vy
+        # else:
+        #     self.vy = -0.7 * self.vy
+
+
     def draw(self):
-        pygame.draw.circle(screen, self.color,(self.x, self.y), self.r)
+        pygame.draw.circle(self.screen, self.color,(self.x, self.y), self.r)
+
+
+class Snitch(Target):
+    def move(self):
+        self.vx=randint(-40,40)
+        self.vy = randint(-40, 40)
+        if ((self.x + self.r) >= 800) and (self.vx>0):
+            self.vx= -self.vx
+        elif ((self.x + self.r) <= 200) and (self.vx<0):
+            self.vx = -self.vx
+        self.x+=self.vx
+        if (((self.y + self.r)>590) and (self.vy<0)):
+            self.vy = -self.vy
+        elif (((self.y + self.r)<10) and (self.vy>0)):
+            self.vy = -self.vy
+        self.y-=self.vy
 
 
 pygame.init()
@@ -186,13 +307,16 @@ balls = []
 
 clock = pygame.time.Clock()
 gun = Gun(screen)
-target = Target()
+target1 = Target(screen)
+target2 = Snitch(screen)
+targets = [target1, target2]
 finished = False
-
+Kspace=0
 while not finished:
     screen.fill(WHITE)
     gun.draw()
-    target.draw()
+    for t in targets:
+        t.draw()
     for b in balls:
         b.draw()
     pygame.display.update()
@@ -204,16 +328,28 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             gun.fire2_start(event)
         elif event.type == pygame.MOUSEBUTTONUP:
-            gun.fire2_end(event)
+            gun.fire2_end(event, Kspace)
         elif event.type == pygame.MOUSEMOTION:
             gun.targetting(event)
+        elif event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_SPACE):
+                if(Kspace):
+                    Kspace=0
+                else:
+                    Kspace=1
 
+
+
+
+    for t in targets:
+        t.move()
     for b in balls:
         b.move()
-        if b.hittest(target) and target.live:
-            target.live = 0
-            target.hit()
-            target.new_target()
+        for t in targets:
+            if b.hittest(t) and t.live:
+                t.live = 0
+                t.hit()
+                t.new_target()
     gun.power_up()
 
 pygame.quit()
